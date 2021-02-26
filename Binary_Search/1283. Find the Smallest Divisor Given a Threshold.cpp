6bb2faa2cb126a016cf876ab590b8a200f77a72b
dk-17 <68947238+dk-17@users.noreply.github.com>
2021-02-26 16:14:38 +0530

    Create 1283. Find the Smallest Divisor Given a Threshold.cpp

---
+++ Binary_Search/1283. Find the Smallest Divisor Given a Threshold.cpp
@@ -0,0 +1,53 @@
+// The range of divisor is from 1 to max element of array element!
+// the reason to choose the right boundary as a max element in the nums because all the divisors greater than equal to the max element will give a result equal to the size of nums!
+
+// consider divisor greater than the element present in the array
+// example [1,2,5,9]
+// suppose the divisor is 20
+// then
+// 1/20+2/20+5/20+9/20
+// 1+1+1+1=4 that is the size of the nums!
+// The minimum value of the threshold can be the size of the nums!
+
+// To increase the result, Decrease divisor
+// To decrese the result, Increse divisor
+
+// if(result<=threshold)
+// then for sure, this can be a possible result so store it in ans variable
+// and decrease range of divisor towards the left i.e(right=mid-1)
+// else
+// since the result value is greater than the threshold it needs to decrease the divisor range towards the right i.e(left=mid+1)!
+
+class Solution {
+public:
+    int Calculate(vector<int>& nums,int divisor){
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=(nums[i]%divisor==0) ? nums[i]/divisor:(nums[i]/divisor)+1;
+        }
+        return sum;
+    }    
+    
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int left=1;
+        int right=0;
+        for(int i=0;i<nums.size();i++)
+            right=max(right,nums[i]);
+        int ans=0;
+        int result=0;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+         
+            result=Calculate(nums,mid);
+            
+            if(result<=threshold){
+                ans=mid;
+                right=mid-1;
+            }
+            else
+                left=mid+1;
+        }
+        return ans;
+        
+    }
+};
